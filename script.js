6e68148caf3223037e6163987a63d628f103d01c
habrasek <65578198+habrasek@users.noreply.github.com>
2021-07-26 18:43:37 -0400

    updating script

--- script.js
+++ script.js
@@ -5,6 +5,32 @@ let response = d3.json(path);
 console.log(response);
 
 
+function init() {
+
+    let path = "data.json";
+    data = d3.json(path);
+    console.log(data);
+
+    let dropdown = d3.select("#selDataset");
+
+        // Read json data
+     d3.json("data.json").then((data) =>{
+         let name = data.data;
+
+            name.forEach((person) =>{
+                dropdown.append("option").text(person["id"]).property("value", person["id"]);
+            })
+
+            let sample = name[0]['id'];
+            console.log(sample);
+
+            build(sample);
+     });
+
+}
+
+    
+
 function build(sample) {
     
     let path = "data.json"
@@ -14,13 +40,14 @@ function build(sample) {
     
     let idInfo = info.data.filter(obj => obj.id == sample);
     console.log(idInfo);
-    console.log(idInfo[0]);
-    console.log(idInfo[0]['name']);
+    //console.log(idInfo[0]);
+    //console.log(idInfo[0]['name']);
 
     let result = idInfo[0];
+    console.log(result);
 
-    console.log(Object.keys(result));
-    console.log(result["songs"]);
+    //console.log(Object.keys(result));
+    //console.log(result["songs"]);
 
     let panel = d3.select("#sample-metadata");
 
@@ -37,11 +64,14 @@ function build(sample) {
     //Object.entries(result).forEach(([key,detail]) => {
     //       panel.append("p").text(`${key}: ${detail}`);
     //   })
-
-
-    
-    
-
-
     }
 )};
+
+
+function optionChanged(thing){
+
+    build(thing);
+   
+}
+
+init();
