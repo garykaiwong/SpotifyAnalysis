12630b781a2a55e998e94fe6bb97e5d5b60bb972
melfraas <73091558+melfraas@users.noreply.github.com>
2021-07-26 21:53:01 -0400

    add sql database

---
+++ Documents/agg_data.sql
@@ -0,0 +1,28 @@
+create table data_o (
+	year INT,
+	artists VARCHAR,
+	explicit INT,
+	id VARCHAR,
+	name VARCHAR,
+	popularity INT,
+	tempo INT
+
+
+);
+
+create table data_by_year (
+	year INT,
+	acousticness INT,
+	danceability INT,
+	duration_ms INT,
+	energy INT,
+	instrumentalness INT,
+	liveness INT,
+	loudness INT,
+	speechiness INT,
+	tempo INT,
+	valence INT,
+	popularity INT,
+	key INT
+
+);
