15a8780777b76b698e1c34d153f09ffe74dd4325
Emily Leniart <81319622+leniartemily@users.noreply.github.com>
2021-08-03 22:51:07 -0400

    adding EL work to github

---
+++ data/data.js
@@ -0,0 +1,81 @@
+let data = [{
+  Song: "Peaches (feat. Daniel Caesar & Giveon)",
+  Artist: "Justin Bieber",
+  Popularity: 100,
+  Danceability: 0.677,
+  Energy: 0.696	,
+  Tempo: 47500
+},
+{
+  Song: "drivers license",
+  Artist: "Olivia Rodrigo",
+  Popularity: 99,
+  Danceability: 0.585,
+  Energy: 0.436,
+  Tempo: 47500
+},
+{
+  Song: "Astronaut In The Ocean	",
+  Artist: "Masked Wolf",
+  Popularity: 98,
+  Danceability: 0.778,
+  Energy: 0.695,
+  Tempo: 47500
+},
+{
+  Song: "Leave The Door Open",
+  Artist:"['Bruno Mars', 'Anderson .Paak', 'Silk Sonic']",
+  Popularity: 96,
+  Danceability: 0.586,
+  Energy: 0.616,
+  Tempo: 47500
+},
+{
+  Song: "Fiel",
+  Artist:"['Los Legendarios', 'Wisin', 'Jhay Cortez']",
+  Popularity: 94,
+  Danceability: 0.849,
+  Energy: 0.701,
+  Tempo: 47500
+},
+{
+  Song: "Friday (feat. Mufasa & Hypeman) - Dopamine Remix",
+  Artist:"['Los Legendarios', 'Wisin', 'Jhay Cortez']",
+  Popularity: 94,
+  Danceability: 0.824,
+  Energy: 0.862,
+  Tempo: 47500
+},
+{
+  Song: "Ella No Es Tuya - Remix	",
+  Artist: "['Rochy RD', 'Myke Towers', 'Nicki Nicole']",
+  Popularity: 92,
+  Danceability: 0.585,
+  Energy: 0.611	,
+  Tempo: 47500
+},
+{
+  Song: "Up",
+  Artist: "['Cardi B']",
+  Popularity: 92,
+  Danceability: 0.868,
+  Energy: 0.795,
+  Tempo: 47500
+},
+{
+  Song: "Goosebumps - Remix",
+  Artist:" ['Travis Scott', 'HVME']",
+  Popularity: 92,
+  Danceability: 0.841	,
+  Energy: 0.593,
+  Tempo: 47500
+},
+{
+  Song: "Hold On",
+  Artist: "Justin Bieber",
+  Popularity: 92,
+  Danceability: 0.658,
+  Energy: 0.634,
+  Tempo: 47500
+}
+];
